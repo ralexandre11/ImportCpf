d52f4a1948fe67ac82ba231653d9a74ca7b9d371
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-12-14 18:08:20 -0300

    Integration test

--- src/main/java/com/ribeiro/ImportCpf/batch/person/PersonItemProcessor.java
+++ src/main/java/com/ribeiro/ImportCpf/batch/person/PersonItemProcessor.java
@@ -1,45 +1,42 @@
 package com.ribeiro.ImportCpf.batch.person;
 
+import java.util.function.Function;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
 
 import com.ribeiro.ImportCpf.domain.Person;
 
+@Component
 public class PersonItemProcessor implements ItemProcessor<Person, Person>{
 	
 	private static final Logger logger = LoggerFactory.getLogger(PersonItemProcessor.class);
+	
+	 private final Function<Person, Boolean> validator;
 
+	 public PersonItemProcessor(final Function<Person, Boolean> validator)
+	 {
+		 this.validator = validator;
+	 }
+	
 	/**
 	 * Responsible to make any necessary change in object data before be writed on database
 	 */
 	@Override
-	public Person process(Person person) throws Exception {
-		if(!isValid(person)) {
+	public Person process(final Person person) throws Exception {
+		if(!validator.apply(person)) {
 			return null;
 		}
-		int id = person.getId();
-		String name = person.getName();
-		String cpf = person.getCpf();
+		final int id = person.getId();
+		final String name = person.getName();
+		final String cpf = person.getCpf();
 		
-		Person transformed = new Person(id,name,cpf);
+		final Person transformed = new Person(id,name,cpf);
 		logger.info("Converting ( {} ) into ( {} )",person,transformed);
 		
 		return transformed;
-	}
-	
-	/**
-	 * Filtering records
-	 * 
-	 * @param person
-	 * @return
-	 */
-	private boolean isValid(Person person) {
-		// Check if CPF is even number
-		if(Long.valueOf(person.getCpf()) % 2 > 0){
-			return false;
-		}
-		return true;
 	}
 	
 }
